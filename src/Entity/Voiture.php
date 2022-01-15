83732c435e9573373001cd12da9f7c9c305b0001
faby-dev <fabyandriamparany@gmail.com>
2022-01-15 09:27:20 +0300

    Ajout de l'entite Commentaire et Voiture

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+#[ApiResource]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Marque;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->Marque;
+    }
+
+    public function setMarque(string $Marque): self
+    {
+        $this->Marque = $Marque;
+
+        return $this;
+    }
+}
